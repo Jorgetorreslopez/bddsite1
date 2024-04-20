5eefc2e614117724ce70968d5829040a7c261aae
Jorgetorreslopez <usda4ever@gmail.com>
2024-04-20 16:45:57 -0500

    Updated Main

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -109,7 +109,7 @@ export function Navbar({ brandName, routes, action }) {
 }
 
 Navbar.defaultProps = {
-  brandName: "Material Tailwind React",
+  brandName: "BDD",
   action: (
     <a
       href="https://www.creative-tim.com/product/material-tailwind-kit-react"
