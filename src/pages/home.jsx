e3cf812ccae64730f2f223c602e6658a114d18f7
Jorgetorreslopez <usda4ever@gmail.com>
2024-04-20 16:30:04 -0500

    Added Video Background

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -19,27 +19,36 @@ export function Home() {
   return (
     <>
       <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <div className="absolute top-0 h-full w-full bg-[url('/img/background-3.png')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
-        <div className="max-w-8xl container relative mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
-              <Typography
-                variant="h1"
-                color="white"
-                className="mb-6 font-black"
-              >
-                Your story starts with us.
-              </Typography>
-              <Typography variant="lead" color="white" className="opacity-80">
-                This is a simple example of a Landing Page you can build using
-                Material Tailwind. It features multiple components based on the
-                Tailwind CSS and Material Design by Google.
-              </Typography>
-            </div>
-          </div>
-        </div>
+  <video
+    className="absolute top-0 h-full w-full object-cover"
+    autoPlay
+    muted
+    loop
+  >
+    <source src="/img/video.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+  <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+  <div className="max-w-8xl container relative mx-auto">
+    <div className="flex flex-wrap items-center">
+      <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+        <Typography
+          variant="h1"
+          color="white"
+          className="mb-6 font-black"
+        >
+          Your story starts with us.
+        </Typography>
+        <Typography variant="lead" color="white" className="opacity-80">
+          This is a simple example of a Landing Page you can build using
+          Material Tailwind. It features multiple components based on the
+          Tailwind CSS and Material Design by Google.
+        </Typography>
       </div>
+    </div>
+  </div>
+</div>
+
       <section className="-mt-32 bg-white px-4 pb-20 pt-4">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
