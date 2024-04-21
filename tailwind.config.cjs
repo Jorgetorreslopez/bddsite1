1591b26d5c545ba9d2c0c467c33533c285a2f37f
joe-bor <kimborlongan_08@yahoo.com>
2024-04-21 16:00:47 -0700

    made the contact form responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    // these are the default values
+    screens: {
+      'sm': '400px',
+      'md': '768px', 
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
   },
   plugins: [],
 });
+
 
